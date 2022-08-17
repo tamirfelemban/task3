056f172d63c0dbb0f2b2714e98f56333c3758cbf
tamirfelemban <109919914+tamirfelemban@users.noreply.github.com>
2022-08-17 13:39:54 +0300

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body style="background-color:#e7d5f7">
+	    
+		<div align="center">
+      
+        <?php
+          echo 'Thanks';
+        ?>
+        </div>
+        
+    </body>
+</html>
